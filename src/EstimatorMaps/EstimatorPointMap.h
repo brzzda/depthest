0b8c415080e6cfe7c57fc4eb6231e205d8192e63
brzzda <garb.zborovsky@gmail.com>
2018-05-30 12:29:23 +0200

    Update EstimatorPointMap.h

--- src/EstimatorMaps/EstimatorPointMap.h
+++ src/EstimatorMaps/EstimatorPointMap.h
@@ -6,7 +6,7 @@
 #define DEPTHEST_POINTFILTERMAP_H
 
 #include <opencv2/opencv.hpp>
-#include "opencv-3.3.1/opencv2/core/mat.hpp"
+#include "opencv2/core/mat.hpp"
 
 /**
  * interface for wrapping Position estimators
